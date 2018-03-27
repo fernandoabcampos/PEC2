a2ee5c7aff5c3c28b49bf94015f6b86d459c6048
Fernando Barbeiro <fernandoabcampos@gmail.com>
2018-03-27 20:16:39 +0200

    Removing unnecessary code

--- exercicio3-discretizacion.R
+++ exercicio3-discretizacion.R
@@ -1,8 +1,3 @@
-#install.packages("discretization")
-#library(discretization)
-library(infotheo)
-
-library(infotheo)
 
 nat <- countries$NATURAL_GROWTH
 
@@ -17,13 +12,4 @@ table(nat_grow_cat)
 nat_grow_cat2=cut(nat, 3)
 table(nat_grow_cat2)
 
-
-
-data(syn.data)
-disc <- "equalwidth"
-nbins <- sqrt(nrow(syn.data))
-ew.data <- discretize(syn.data, disc, nbins)
-
-table(discretize(countries, "cluster", categories=3))
-help.search("discretize")
-
+barplot(table(nat_grow_cat))
